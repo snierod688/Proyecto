f8674540ec7e89286717c05eff634af148d19398
snierod688 <91082499+snierod688@users.noreply.github.com>
2023-04-21 18:10:40 +0200

    Diseño de pantalla de login y registro

---
+++ EnerGym/app/src/main/java/pmdm/t4/ener_gym/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package pmdm.t4.ener_gym.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pmdm.t4.ener_gym.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
