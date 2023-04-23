ac867e210f676d1adb6323c4237c1f6f61ec3b8c
ACDAT_2233 <snierod688@iesmartinezm.es>
2023-04-23 18:47:33 +0200

    comienzo de codificación de la pantalla de Login y de Registro

--- EnerGym/app/src/main/java/pmdm/t4/ener_gym/login/LoginActivity.java
+++ EnerGym/app/src/main/java/pmdm/t4/ener_gym/login/LoginActivity.java
@@ -1,16 +1,82 @@
 package pmdm.t4.ener_gym.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import pmdm.t4.ener_gym.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import pmdm.t4.ener_gym.MainActivity;
+import pmdm.t4.ener_gym.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Se inicializa la instancia de Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        iniciarSesion();
+        registro();
     }
+
+    private void iniciarSesion() {
+
+        String username = binding.etUsername.getText().toString().trim();
+        String password = binding.etPassword.getText().toString().trim();
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Snackbar.make(binding.loginActivity, "Hay campos vacíos", Snackbar.LENGTH_LONG).show();
+                } else {
+                    login(username, password);
+
+                }
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Snackbar.make(binding.loginActivity, "BIENVENIDO " + username, Snackbar.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Snackbar.make(binding.loginActivity, "El usuario no existe\n¡REGISTRESE!", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void registro(){
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
--- EnerGym/app/src/main/java/pmdm/t4/ener_gym/login/RegisterActivity.java
+++ EnerGym/app/src/main/java/pmdm/t4/ener_gym/login/RegisterActivity.java
@@ -3,14 +3,46 @@ package pmdm.t4.ener_gym.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-import pmdm.t4.ener_gym.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import pmdm.t4.ener_gym.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Se inicializan la instancias de Firebase
+        mFirebase = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        comprobarCampos();
+    }
+
+    private void comprobarCampos() {
+
+        String username = binding.etUsername.getText().toString().trim();
+        String nombre = binding.etName.getText().toString();
+        String correo = binding.etEmail.getText().toString().trim();
+        String password = binding.etPassword.getText().toString().trim();
+        String confPassword = binding.etPasswordConfirm.getText().toString().trim();
+
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
